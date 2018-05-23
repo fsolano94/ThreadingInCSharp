b5b577fd0e8732a6c0f0f3e8a22c900979cf93e1
Franky Solano <fsolano@nevada.unr.edu>
2018-05-22 20:06:41 -0700

    initial source code commit

---
+++ Threading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        public bool messageDisplayed = false;
+        public object locker = new object();
+
+        static void Main(string[] args)
+        {
+            Program programInstance = new Program();
+
+            Thread t1 = new Thread(()=> programInstance.DisplayMessage("Learning Threading using C#. From Thread 1."));
+            Thread t2 = new Thread(() => programInstance.DisplayMessage("Learning Threading using C#. From Thread 2."));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Console.ReadLine();
+        }
+
+        public  void DisplayMessage(string message)
+        {
+            lock(locker)
+            {
+                if (!messageDisplayed)
+                {
+                    Console.WriteLine(message);
+                    messageDisplayed = true;
+                }
+            }
+            
+        }
+
+    }
+}
